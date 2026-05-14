662291e7bc56b72ca62c662663fa131350ec3156
Vaibhav-Pacherwal <vaibhavpacherwal57@gmail.com>
2026-05-14 18:58:39 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/api/v1", chatRoutes);
